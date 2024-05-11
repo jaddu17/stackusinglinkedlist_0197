28bc661d367a752ef3bdb7f64e493644941e8fca
jaddu17 <shafajaddu@gmail.com>
2024-05-11 09:49:28 +0700

    menetapkan nilai

--- StackLinkedList/StackLinkedList.cpp
+++ StackLinkedList/StackLinkedList.cpp
@@ -25,6 +25,7 @@ public:
     // Push Operation: Insert an element onto the top of the stack
     int push(int value) {
         Node* newNode = new Node();     // 1. Allocate memory for the new node
+        newNode->data = value;          // 2. assign value
     }
 };
 
