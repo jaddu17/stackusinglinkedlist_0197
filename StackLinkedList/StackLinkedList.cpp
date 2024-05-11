55c246bcad01eac97bb81ca78d6dc1d51b447aa6
jaddu17 <shafajaddu@gmail.com>
2024-05-11 09:41:56 +0700

    node class yang mewakili single node dalam linked list

--- StackLinkedList/StackLinkedList.cpp
+++ StackLinkedList/StackLinkedList.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+// Node class representing a single node in the linked list
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node() {
+        next = NULL;
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
