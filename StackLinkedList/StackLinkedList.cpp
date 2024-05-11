a81e8729ee55670ca61f4fe3ac4ad574b64bf404
jaddu17 <shafajaddu@gmail.com>
2024-05-11 09:46:45 +0700

    stack dgn top null

--- StackLinkedList/StackLinkedList.cpp
+++ StackLinkedList/StackLinkedList.cpp
@@ -16,6 +16,11 @@ public:
 class Stack {
 private:
     Node* top;      // Pointer to the top node of the stack
+
+public:
+    Stack() {
+        top = NULL;     // Initialize the stack with a null top pointer
+    }
 };
 
 int main()
