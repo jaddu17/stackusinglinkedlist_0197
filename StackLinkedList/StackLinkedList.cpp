0613052db877c779e13075ca7fc7cca7940bf9cf
jaddu17 <shafajaddu@gmail.com>
2024-05-11 10:03:48 +0700

    Return the value of the top node

--- StackLinkedList/StackLinkedList.cpp
+++ StackLinkedList/StackLinkedList.cpp
@@ -46,6 +46,15 @@ public:
         if (top == NULL) {
             cout << "List is empty." << endl;
         }
+        else {
+            Node* current = top;
+            while (current != NULL) {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+        // Return the value of the top node
     }
 };
 
